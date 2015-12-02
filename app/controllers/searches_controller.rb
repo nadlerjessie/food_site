ee54ccfd2ca47b6ccf326873ae497eae690b7f80
danielglatstein <danielglatstein@gmail.com>
2015-12-02 17:38:15 -0500

    add awesome advanced sql to search based on user permissions

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,8 +1,9 @@
 class SearchesController < ApplicationController
   skip_before_action :login_required
   def index
+    user_logged_in = current_user
     recipe = params[:keyword]
-    @recipes = Search.for(recipe)
+    @recipes = Search.for(recipe, user_logged_in)
     if @recipes.size == 1
       recipe = @recipes.first
       redirect_to recipe_path(recipe)
--- app/models/search.rb
+++ app/models/search.rb
@@ -1,9 +1,43 @@
 class Search < ActiveRecord::Base
-  def self.for(recipe)
+  def self.for(recipe, current_user)
     search = "%#{recipe.downcase}%"
-    permissions = true
-    x = Recipe.where("lower(name) like ? and public_recipe = ?", search, permissions)
+    binding.pry
+    search_based_on_user_status(search, current_user)
   end
+
+  def search_based_on_user_status(search, current_user)
+    if logged_in?
+      Recipe.where("(lower(name) like ? AND public_recipe = ?) OR (lower(name) like ? AND user_id = ?)", search, true, search, current_user.id)
+    else
+      Recipe.where("lower(name) like ? and public_recipe = ?", search, true)
+    end
+  end
+
 end
 
-#search by ingredient, category, by user who created if public = false
+
+
+
+
+
+
+# select the recipe where the name matches search and the public_status is true OR a recipe whose name matches search and the recipe.user_id is equal to the current user
+#
+#   def current_user_permissions?
+#     if logged_in?
+#       true || false
+#     else
+#       true
+#     end
+#   end
+#
+#   def recipe_permissions?
+#   end
+#
+# end
+
+#search by ingredient, category, by user who created if
+
+
+
+# method to hand the logic for permissions
