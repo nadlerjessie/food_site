b42fd25982f13a22427f7b238bebe552b76b2cd8
nadlerjessie <nadler.jessie@gmail.com>
2015-12-03 20:03:32 -0500

    added sessions test

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,11 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe SessionsController, type: :controller do
-  before do
-    visit '/'
-  end  
+  describe "/ when not logged in" do
+    before do
+      visit '/'
+    end  
 
-  it 'expects login link to appear' do
-    expect(page.should have_selector(:link_or_button, 'Login'))
+    it 'expects login link to appear' do
+      expect(page).to have_selector(:link_or_button, 'Login')
+    end
+  end
+
+  describe "logging in" do
+    before do
+      visit '/'
+    end  
+
+    it 'clicks Login and goes to /login' do
+      click_on('Login')
+      expect(current_path).to eq(login_path)
+    end
+
+    # it 'can login and see name and logout link' do
+    #   visit '/login'
+    #   fill_in('login-email'), with: ''
+    #   expect(page).to have_selector(:link_or_button, 'Login')
+    # end
   end
 end
