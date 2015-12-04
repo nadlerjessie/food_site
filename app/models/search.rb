73de93d31c6dfa870be94f6774b1cf55f3e5d55b
mgsterling11 <m.g.sterling11@gmail.com>
2015-12-04 15:16:32 -0500

    Removes categories from search methods:
    Cleans up _nav form to include current user
    
    removes other bugs

--- app/models/search.rb
+++ app/models/search.rb
@@ -1,16 +1,12 @@
 class Search < ActiveRecord::Base
   def self.for(recipe, current_user)
     search = "%#{recipe.downcase}%"
-    binding.pry
     search_by_user_permissions(search, current_user)
   end
 
   def self.search_by_user_permissions(search, current_user)
-    # if current_user
-      Recipe.joins(:ingredients).joins(:categories).where("((lower(recipes.name) like ? OR ingredients.name like ? OR categories.name like ?) AND public_recipe = ?) OR ((lower(recipes.name) like ? OR ingredients.name like ? OR categories.name like ?) AND user_id = ?)", search, search, search, true, search, search, search, current_user.id).uniq
-    # else
-    #   Recipe.joins(:ingredients).joins(:categories).where("(lower(recipes.name) like ? OR ingredients.name like ? OR categories.name like ?) AND public_recipe = ?", search, search, search, true).uniq
-    # end
+    Recipe.joins(:ingredients).where("((lower(recipes.name) like ? OR ingredients.name like ? ) AND public_recipe = ?) OR ((lower(recipes.name) like ? OR ingredients.name like ? ) AND user_id = ?)", search, search, true, search, search, current_user.id).uniq
+    
   end
 
 end
