43d68a1011b2664946ae0f6c545f42c85319727b
danielglatstein <danielglatstein@gmail.com>
2015-12-04 22:16:08 -0500

    adjust indents on search model

--- app/models/search.rb
+++ app/models/search.rb
@@ -6,6 +6,5 @@ class Search < ActiveRecord::Base
 
   def self.search_by_user_permissions(search, current_user)
     Recipe.joins(:ingredients).where("((lower(recipes.name) like ? OR ingredients.name like ? ) AND public_recipe = ?) OR ((lower(recipes.name) like ? OR ingredients.name like ? ) AND user_id = ?)", search, search, true, search, search, current_user.id).uniq
-    
   end
 end
