d0d370d6d4fb1a7aba4d658ba8275242798d248d
nadlerjessie <nadler.jessie@gmail.com>
2015-12-07 13:44:31 -0500

    added javascript to hide and remove form for double click on steps

--- app/assets/javascripts/steps.js
+++ app/assets/javascripts/steps.js
@@ -1,5 +1,13 @@
 $(function(){
-   $(document).on('ajax:success', '.edit_step', function(e, data, status, xhr){
-   $(this).parent().children('.step-js').html(data.template)
+  $(document).on('ajax:success', '.edit-step-js', function(e, data, status, xhr){
+    $(this).parent().children('.step-js').html(data.template)
+    $(this).addClass('hide')
+    $(this).parent().children('.step-js').removeClass('hide')
+
+  })
+
+  $(document).on('dblclick', '.step-description-js', function() {
+    $(this).children('.edit-step-js').removeClass('hide')
+    $(this).children('.step-js').addClass('hide')
   })
 })
