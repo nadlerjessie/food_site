319eeeee08786a60a447ea81d97dea91b7a1b383
danielglatstein <danielglatstein@gmail.com>
2015-12-07 19:45:29 -0500

    prevent new proportion field from appearing when hitting enter on description for new recipe form

--- app/assets/javascripts/controllers/descriptions.controller.js
+++ app/assets/javascripts/controllers/descriptions.controller.js
@@ -6,11 +6,15 @@ $(function(){
   var value = $('.description-item').last().data('description-id') + 1
   var html = "<div class='description-item' data-description-id=" + value + ">"
       html += $('.description-item').html();
-      html += "</div>"    
+      html += "</div>"
       html = html.replace(/\d+/g, value)
-  
+
     $('.descriptions-list').append(html);
 
   })
+
+  $('.description-item').on("keypress", function(event) {
+    return event.keyCode != 13;
+  });
 
 });
