c13e67fe88fd3ec33e09523813de28cbdf2cbfa5
mgsterling11 <m.g.sterling11@gmail.com>
2015-12-08 14:06:17 -0500

    fully functioning AJAX when logged in or out with user permissions attached to editing capabilities

--- app/assets/javascripts/controllers/proportions.controller.js
+++ app/assets/javascripts/controllers/proportions.controller.js
@@ -31,8 +31,7 @@ $(function(){
   });
 
   // $(document).on('ajax:success', '.edit-proportion-js', function(e, data, status, xhr){
-  //   debugger;
-  //   $('.proportion-show-partial-js').replaceWith($(data.template))
+  //   $('.proportion-show-partial-js').empty().append($(data.template))
   // })
 });
 
--- app/assets/javascripts/steps.js
+++ app/assets/javascripts/steps.js
@@ -1,6 +1,5 @@
 $(function(){
   $(document).on('ajax:success', '.edit-step-js', function(e, data, status, xhr){
-    debugger;
     $(this).parent().children('.step-js').html(data.template)
     $(this).addClass('hide')
     $(this).parent().children('.step-js').removeClass('hide')
@@ -9,5 +8,9 @@ $(function(){
   $(document).on('dblclick', '.step-description-js', function() {
     $(this).children('.edit-step-js').removeClass('hide')
     $(this).children('.step-js').addClass('hide')
+  })
+
+  $(document).on('ajax:success', '.edit-step-js', function(e, data, status, xhr){
+    $('.step-show-partial-js').empty().append($(data.template))
   })
 })
