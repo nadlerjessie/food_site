23cd3c5eac2ff4fee35b479a548a77772fe2a067
mgsterling11 <m.g.sterling11@gmail.com>
2015-12-09 12:08:49 -0500

    commiting additional changes, closer to working version

--- app/assets/javascripts/categories.js
+++ app/assets/javascripts/categories.js
@@ -1,11 +1,25 @@
 $(document).ready(function() {
   // categories-checkboxs.val()
+
   $('input:checkbox').change(function(){
-    // $('.photo-grid').hide();
-    $('input:checkbox:not(:checked)').each(function(index, not_checked){
-      debugger;
-      $('li').filter(not_checked.value).hide()
-      //return $(this).val()
+    // displays all (hidden) recipies 
+    $('.photo-grid li').hide();
+    // $('input:checkbox:not(:checked)').each(function(index, not_checked){
+    //   debugger;
+      // $('li').filter(not_checked.value).hide()
+      // //return $(this).val()
+    var checked = $('input:checkbox:checked').map(function(index, checked){
+      return "." + checked.value
     })
+
+    $.each(checked, function(id){
+      $('li').filter(id).show();
+    })
+
+
+    // $('input:checkbox:checked').each(function() {
+    //   $('li').filter("." + $(this).val()).show();
+    // });
+
   })
 });
