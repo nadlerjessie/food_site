52ec57d981960bc9bca41ac663e6c36d88a9950a
nadlerjessie <nadler.jessie@gmail.com>
2015-12-09 13:13:54 -0500

    removed unnecessary console.log statements

--- app/assets/javascripts/categories.js
+++ app/assets/javascripts/categories.js
@@ -3,7 +3,6 @@ $(document).ready(function() {
   $('input:checkbox').change(function(){
     if ($('input:checkbox:checked').length == 0) {
       $('.photo-grid li').show();
-      console.log($('.photo-grid li:visible').length)
     }
     else {
       $('.photo-grid li').hide();
@@ -14,7 +13,6 @@ $(document).ready(function() {
       $.each(checked, function(index, category_id){
         $('li').filter(category_id).show();
       });
-      console.log($('.photo-grid li:visible').length)
     };
   });
 
