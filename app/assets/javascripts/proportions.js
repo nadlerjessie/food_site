027f75aa95c19d07473694c680bec3d99e902028
AmandaKJohns <amandajohns77@gmail.com>
2015-12-09 09:01:37 -0500

    move description and proportion controllers to javascript directory

--- app/assets/javascripts/proportions.js
+++ app/assets/javascripts/proportions.js
@@ -1,7 +1,23 @@
 $(function(){
-  // $(document).on('click', 'button.delete-proprtion-js', function() {
-  //   debugger;
-  // })
+ 
+ /// New Form
+$(document).on('click', '.add-proportion', function(event){
+    event.preventDefault();
+  var value = $('.proportion-item').last().data('proportion-id') + 1
+  var html = "<div class='proportion-item' data-proportion-id=" + value + ">"
+      html += $('.proportion-item').html();
+      html += "</div>"
+      html = html.replace(/\d+/g, value)
+
+    $('.proportions-list').append(html);
+
+  })
+
+$(document).on("keypress", '.proportion-item', function(event) {
+    return event.keyCode != 13;
+  });
+
+ /// Edit Form
   function resetPlaceholder () {
     $('.quantity-input-js').attr("placeholder", 'Quantity')
   };
@@ -28,5 +44,23 @@ $(function(){
     $(this).children('.edit-proportion-js').removeClass('hide')
     $(this).children('.proportion-js').addClass('hide')
   })
+
+  $(document).on('ajax:success', '.add-proportion-from-show-js', function(e, data, status, xhr){
+    var quantity = data.quantity;
+    var unit = data.unit;
+    var ingredient = data.ingredient;
+    var cloned_proportion = $('.proportion-js').clone().get(0);
+    var proportion_html = $(cloned_proportion).find('li').text(data.quantity + " " + data.unit + " " + data.ingredient);
+    $('.proportion-show-partial-js').append($(proportion_html));
+    $('li').last().wrap("<ul class='proportion-list-js'><div class='proportion-js'></div></ul>");
+    var form_fields = $('.add-proportion-from-show-js').find('input:text');
+    form_fields.first().val("");
+    $(form_fields.get(1)).val("");
+    form_fields.last().val("");
+  });
+
+  $(document).on('click', '.display-add-ingredient-form-js', function(){
+    $('.add-proportion-from-show-js').removeClass('hide')
+  });
 
 })
