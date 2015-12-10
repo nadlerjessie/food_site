923ae49f0c4b895b61ee04880d8668e94afdacca
AmandaKJohns <amandajohns77@gmail.com>
2015-12-10 13:56:06 -0500

    create view object for state_season and map view

--- app/controllers/states_controller.rb
+++ app/controllers/states_controller.rb
@@ -3,19 +3,27 @@ class StatesController < ApplicationController
   def map
   end
 
+
   def data
-   state_name = params['name'].split('-').map { |word| word.capitalize }
-   state_name = state_name.join('-')
-   state = State.find_by(name: state_name)
-   state_name_view = state.name.gsub('-', ' ')
-   month = Time.now.month.to_s
-   month_name = Time.now.strftime("%B")
-   current_seasons = Season.where(month: month)
-   state_seasons = StateSeason.where(state: state, season: current_seasons)
+   state = find_selected_state(params['name'])
+   state_seasons = find_current_state_seasons(state)
    ingredients = StateSeason.this_months_ingredients(state_seasons)
-   html_string = render_to_string(partial: "states/seasons", locals: {:state_name => state_name_view, :month => month_name, :ingredients => ingredients})
+   html_string = render_to_string(partial: "states/seasons", locals: {:state => state, :ingredients => ingredients})
    render json: {template: html_string}
   end
 
+private
+
+  def find_selected_state(state)
+    state_name = state.split('-').map { |word| word.capitalize }
+    state_name = state_name.join('-')
+    State.find_by(name: state_name)
+  end
+
+  def find_current_state_seasons(state)
+    month = Time.now.month.to_s
+    current_seasons = Season.where(month: month)
+    StateSeason.where(state: state, season: current_seasons)
+  end
 
 end
