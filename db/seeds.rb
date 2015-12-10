6c60e684905e6545478ddbfca1dba9f3934b3540
nadlerjessie <nadler.jessie@gmail.com>
2015-12-10 17:07:38 -0500

    updated adapter to build valid proportions after addition of custom validation

--- db/seeds.rb
+++ db/seeds.rb
@@ -275,6 +275,7 @@ food_network_recipe = Recipe.new.create_recipe_from_food_network_adapter("http:/
 get_food_network_links = Adapters::FoodNetworkLinks.new.page_navigation
 get_food_network_links.each do |link|
   @recipe = Recipe.new
+
   @recipe.create_recipe_from_food_network_adapter("http://www.foodnetwork.com#{link}")
 end
 
