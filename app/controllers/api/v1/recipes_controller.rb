9671f68f1e9b08efe401bfa62b94cbea0af330a1
nadlerjessie <nadler.jessie@gmail.com>
2015-12-11 09:56:27 -0500

    added recipe show to controller

--- app/controllers/api/v1/recipes_controller.rb
+++ app/controllers/api/v1/recipes_controller.rb
@@ -3,4 +3,9 @@ class Api::V1::RecipesController < ApplicationController
     @recipes = Recipe.all 
     render json: @recipes
   end
+
+  def show
+    @recipe = Recipe.find(params[:id])
+    render json: @recipe
+  end
 end
